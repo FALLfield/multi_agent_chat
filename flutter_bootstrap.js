98748dc5d0acfead0543ec4a7f0f5fbc60344d1a
FALLfield <FALLfield@users.noreply.github.com>
2026-04-07 15:19:24 +0000

    deploy: 5ec32eecef17178535815c85a2e1fa546cb2920d

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "600359512" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4230053902" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
